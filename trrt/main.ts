bd6443dfd4418194e4ee762d9583a4e33dad4623
Chadha Degachi <chadha.degachi@tno.nl>
2022-10-14 17:08:50 +0200

    init re issue #1

---
+++ trrt/main.ts
@@ -0,0 +1,12 @@
+const directory_path: string = "docs\\terms";
+
+function read_dir(path : string){
+      console.log(`Reading files at: ${path}`);
+}
+
+
+function main(){
+      read_dir(directory_path);
+}
+
+main();
---
+++ trrt/readme.md
@@ -0,0 +1,2 @@
+# TRRT
+A tool for [Term Reference Resolution](https://essif-lab.github.io/framework/docs/tev2/spec-tools/trrt)
