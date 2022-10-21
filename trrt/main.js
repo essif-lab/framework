e04fdfe22741ebd352e93f566252ff3b6b63c573
Chadha Degachi <chadha.degachi@tno.nl>
2022-10-21 15:55:47 +0200

    should resolve #1

--- trrt/main.js
+++ trrt/main.js
@@ -8,13 +8,15 @@ var fs = require("fs");
 var path = require("path");
 var directory_path = "C:\\Users\\degachic\\Documents\\workspace\\trrt\\framework-trrt\\docs\\terms\\";
 var output_path = "C:\\Users\\degachic\\Documents\\workspace\\trrt\\framework-trrt\\docs\\terms_updated\\";
-var term_regex = new RegExp("(?<=[^`\\])\[(?=[^@\]]+\]\([#a-z0-9_-]*@[:a-z0-9_-]*\))");
+var term_regex = /(?<=[^`\\])\[(?=[^@\]]+\]\([#a-z0-9_-]*@[:a-z0-9_-]*\))(?<showtext>.+?)\]\((?<id>[a-z0-9_-]+?)(?:#(?<trait>[a-z0-9_-]+?))?@(?<scopetag>[a-z0-9_-]*)(?::(?<vsntag>[a-z0-9_-]+?))?\)/g;
 function resolve_terms(directory, output) {
     show_status("Reading " + directory + ".....");
-    fs.mkdir(output, function (err) {
-        if (err)
-            throw err;
-    });
+    if (!fs.existsSync(output)) {
+        fs.mkdir(output, function (err) {
+            if (err)
+                throw err;
+        });
+    }
     fs.readdir(directory, function (err, files) {
         if (err)
             show_status(" \t " + err);
@@ -34,7 +36,7 @@ function read_file(directory, file, output) {
         }
         else {
             show_status("Reading: " + file);
-            data = replace_terms(data);
+            data = replace_terms(data, directory);
             write_file(output, file, data);
         }
     });
@@ -49,8 +51,60 @@ function write_file(output, file, data) {
         }
     });
 }
-function replace_terms(data) {
-    return data.replace(term_regex, "placeholder");
+function replace_terms(data, term_path) {
+    // good example term.md
+    var matches = data.matchAll(term_regex);
+    // basic syntax, i.e. [showtext](term#trait@scopetag:vsntag);
+    // alternative syntax, e.g. [show text@]
+    // markdown output: [showtext](current-path/term#trait)
+    for (var _i = 0, _a = Array.from(matches); _i < _a.length; _i++) {
+        var match = _a[_i];
+        var showtext = "";
+        var id = "";
+        var term = "";
+        var trait = "";
+        var scopetag = "";
+        var vsntag = "";
+        if (match.groups.showtext != undefined) {
+            showtext = match.groups.showtext;
+        }
+        else {
+            showtext = "Missing Input";
+        }
+        if (match.groups.id != undefined) {
+            id = match.groups.id;
+        }
+        else {
+            id = "Missing Input";
+        }
+        if (match.groups.term != undefined) {
+            term = match.groups.trait;
+        }
+        else {
+            term = "Missing Input";
+        }
+        if (match.groups.trait != undefined) {
+            trait = match.groups.trait;
+        }
+        else {
+            trait = "Default";
+        }
+        if (match.groups.scopetag != undefined) {
+            scopetag = match.groups.scopetag;
+        }
+        else {
+            scopetag = "default";
+        }
+        if (match.groups.vsntag != undefined) {
+            vsntag = match.groups.vsntag;
+        }
+        else {
+            vsntag = "latest";
+        }
+        var markdown_out = "[".concat(showtext, "](").concat(term_path, "/").concat(id, "/").concat(trait, "/").concat(scopetag, "/").concat(vsntag, ")");
+        data = data.replace(term_regex, markdown_out);
+    }
+    return data;
 }
 function show_status(status) {
     // for working locally
@@ -61,5 +115,6 @@ function show_status(status) {
 }
 function main() {
     resolve_terms(directory_path, output_path);
+    // console.log(replace_terms("these are the acts [the purpose of actors](actor#purpose@essif-lab) and so on [the purpose of actors] and so on @essif-lab [the purpose of actors](actor#purpose@essif-lab)", "/current_path"));
 }
 main();
--- trrt/main.ts
+++ trrt/main.ts
@@ -15,9 +15,11 @@ const term_regex = /(?<=[^`\\])\[(?=[^@\]]+\]\([#a-z0-9_-]*@[:a-z0-9_-]*\))(?<sh
 function resolve_terms(directory: string, output: string) {
       show_status("Reading " + directory + ".....");
 
-      fs.mkdir(output, (err) => {
-            if (err) throw err;
-      });
+      if (!fs.existsSync(output)) {
+            fs.mkdir(output, (err) => {
+                  if (err) throw err;
+            });
+      }
 
       fs.readdir(directory, (err: Error, files: Array<string>) => {
             if (err) show_status(" \t " + err);
@@ -37,7 +39,7 @@ function read_file(directory: string, file: string, output: string) {
                   show_status(" \t " + err);
             } else {
                   show_status("Reading: " + file);
-                  data = replace_terms(data);
+                  data = replace_terms(data, directory);
                   write_file(output, file, data);
             }
       });
@@ -53,20 +55,54 @@ function write_file(output: string, file: string, data: string) {
       });
 }
 
-function replace_terms(data: string): string {
+function replace_terms(data: string, term_path: string): string {
       // good example term.md
       const matches: IterableIterator<RegExpMatchArray> = data.matchAll(term_regex);
       // basic syntax, i.e. [showtext](term#trait@scopetag:vsntag);
       // alternative syntax, e.g. [show text@]
       // markdown output: [showtext](current-path/term#trait)
       for (const match of Array.from(matches)) {
-            console.log("Match")
-            console.log(match.groups.showtext);
-            console.log(match.groups.id);
-            console.log(match.groups.trait);
-            console.log(match.groups.scopetag);
-            console.log(match.groups.vsntag);
-            // data = data.replace(match.groups.showtext, "placeholder");
+            var showtext: string = "";
+            var id: string = "";
+            var term: string = "";
+            var trait: string = "";
+            var scopetag: string = "";
+            var vsntag: string = "";
+
+            if (match.groups.showtext != undefined) {
+                  showtext = match.groups.showtext;
+            } else {
+                  showtext = "Missing Input";
+            }
+            if (match.groups.id != undefined) {
+                  id = match.groups.id;
+            } else {
+                  id = "Missing Input";
+            }
+            if (match.groups.term != undefined) {
+                  term = match.groups.trait;
+            } else {
+                  term = "Missing Input";
+            }
+            if (match.groups.trait != undefined) {
+                  trait = match.groups.trait;
+            } else {
+                  trait = "Default";
+            }
+            if (match.groups.scopetag != undefined) {
+                  scopetag = match.groups.scopetag;
+            } else {
+                  scopetag = "default";
+            }
+            if (match.groups.vsntag != undefined) {
+                  vsntag = match.groups.vsntag
+            } else {
+                  vsntag = "latest"
+            }
+
+            var markdown_out: string = `[${showtext}](${term_path}/${id}/${trait}/${scopetag}/${vsntag})`;
+
+            data = data.replace(term_regex, markdown_out);
       }
 
       return data;
@@ -82,8 +118,8 @@ function show_status(status: string): void {
 }
 
 function main(): void {
-      // resolve_terms(directory_path, output_path);
-      console.log(replace_terms("these are the acts [the purpose of actors](actor#purpose@essif-lab) and so on [the purpose of actors] and so on @essif-lab [the purpose of actors](actor#purpose@essif-lab)"));
+      resolve_terms(directory_path, output_path);
+      // console.log(replace_terms("these are the acts [the purpose of actors](actor#purpose@essif-lab) and so on [the purpose of actors] and so on @essif-lab [the purpose of actors](actor#purpose@essif-lab)", "/current_path"));
 }
 
 main();
